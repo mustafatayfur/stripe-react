c5cb1d8c97788e4a1ebbe3cdb430341ea41caf0f
mustafa <mltm0204@gmail.com>
2021-12-28 14:54:52 +0300

    codes are added

--- src/context.js
+++ src/context.js
@@ -6,8 +6,10 @@ const AppContext = createContext();
 
 export const AppProvider = ({children}) => {
 
-const [isSidebarOpen, setIsSidebarOpen] = useState(true)
-const [isSubmenuOpen, setIsSubmenuOpen] = useState(true)
+const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+const [isSubmenuOpen, setIsSubmenuOpen] = useState(false)
+const [location, setLocation] = useState({})
+const [page, setPage] = useState({page: '', links:[]})
 
 const openSidebar = ()=>{
     setIsSidebarOpen(true)
@@ -15,11 +17,14 @@ const openSidebar = ()=>{
 const closeSidebar = ()=>{
     setIsSidebarOpen(false)
 }
-const openSubmenu = ()=>{
-    setIsSubmenuOpen(true)
-}
 const closeSubmenu = ()=>{
     setIsSubmenuOpen(false)
+}
+const openSubmenu = (text, coordinates)=>{
+    const page = sublinks.find((link)=> link.page===text)
+    setPage(page)
+    setLocation(coordinates)
+    setIsSubmenuOpen(true)
 }
 
 return (
@@ -29,7 +34,9 @@ return (
         openSubmenu,
         openSidebar,
         closeSubmenu,
-        closeSidebar
+        closeSidebar,
+        location, 
+        page
     }}>
         {children}
     </AppContext.Provider>
